22b4857aa08b8c05125216ace1ebcb15472f30a3
Ali <alimecha60@gamil.com>
2022-11-25 23:08:02 +0530

    second commit

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("btn").addEventListener("click",function(){
+  let name=  document.getElementById("name").value
+ let email=   document.getElementById("name").value
+  let phone=  document.getElementById("name").value
+  if(name!=="" && email !=="" && phone!=="" ){
+    alert(`Thank-you we will contact you soon`);};
+})
